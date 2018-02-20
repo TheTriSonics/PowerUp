378d555facfdee4e0826146232e8dc80e65e2a8a
davidaustinm <davidaustinm@gmail.com>
2018-02-19 21:51:24 -0500

    small fix

--- src/org/usfirst/frc/team4003/robot/state/StateCommand.java
+++ src/org/usfirst/frc/team4003/robot/state/StateCommand.java
@@ -23,7 +23,7 @@ public class StateCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	System.out.println("executing");
+    	//System.out.println("executing");
     }
 
     // Make this return true when this Command no longer needs to run execute()
